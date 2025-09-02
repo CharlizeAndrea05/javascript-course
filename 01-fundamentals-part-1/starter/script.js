9a20cc27e172a8af8ea24f1a5f3f06440e25f1b2
Charlize <hellocharlizekitty@gmail.com>
2025-09-03 02:11:05 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,74 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log(js);
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+console.log(age);
+age = 50; 
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+// do not do this
+// birthYear = 2004;
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+
+const country = "Philippines";
+const language = "Filipino";
+
+age = 25;
+age = 26; // this will change
+
+// let firstName = "Sarah";
+// let myCurrentJob = "teacher";
+// let PI = 3.1415; 
+
+console.log("=== DATA TYPES ===");
+
+// Number 
+let id = 12345;
+console.log(id);
+console.log(typeof id);
+
+// String 
+let lastName = "Doe";
+console.log(lastName);
+console.log(typeof firstName);
+
+// Boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// Undefined 
+let year;
+console.log(year);
+console.log(typeof year);
+
+let dynamicVariable = 40;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+// Change to a string
+dynamicVariable = "I'm now a string!";
+console.log(dynamicVariable, typeof dynamicVariable); 
+
+// Change to a boolean
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
---
+++ 02-fundamentals-part-2/starter/script.js
@@ -0,0 +1,182 @@
+// console.log("Part 2 functions is ready. ");
+
+// // Functions - Declarations and Expressions
+// function logger() {
+//     console.log("My name is Charlize");
+// }
+
+// logger ();
+// logger ();
+// logger ();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(5, 3));
+
+// const juice1 = fruitProcessor(5, 0);
+// const juice2 = fruitProcessor(2, 4);
+// const juice3 = fruitProcessor(3, 2);
+
+// // Function expression
+// const calcAge = function (birthYear) {
+//     return 2025 - birthYear;
+// };
+
+// const age1 = calcAge(2004);
+// console.log(age1);
+
+// console.log(calcAge(2004));
+
+// function introduce(firstName, lastName, age) {
+//     const introduction = `Hi, I'm ${firstName} ${lastName} and I'm ${age} years old`; 
+//     return introduction;
+// };
+
+// console.log(introduce("Charlize", "Jaromay", 21));
+// console.log(introduce("John"));
+
+// function yearsUntilRetirement(birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         return `${firstName} retires in ${retirement} years`;
+//     } else {
+//         return `${fisrtName} has already retired`;
+//     }
+// };
+
+// console.log(yearsUntilRetirement(2004, "Charlize"));
+
+// // function scope
+// const globalVar = "I am global";
+
+// function testScope () {
+//     const localVar = "I am global";
+//     console.log(globalVar);
+//     console.log(localVar);
+// };
+
+// testScope();
+// console.log(globalVar);
+
+
+// ////////////////////////////////////
+// // Coding Challenge #1
+
+// // Function to calculate average of 3 scores
+// function calcAverage(score1, score2, score3) { 
+//     return (score1 + score2 + score3) /3 ;
+// };
+
+// // Function to check winner
+// function checkWinner(avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     return `Dolphins win ${avgKoalas} vs. ${avgDolphins}`; 
+//     } else {
+
+//     }
+//   }
+
+
+// // Test Data 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// // Test Data 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+const grades = [85, 92, 78, 96, 88];
+console.log(grades);
+
+const friends = [""];
+console.log(friends);
+
+const mixed =["Jonas", 40, false, friends];
+console.log(mixed);
+
+const years = new Array(1991, 1994, 2008, 2020);
+console.log(years);
+
+//Accessing Arrays
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(friends[3]);
+
+// access the length
+console.log(friends.length);
+
+friends[0] = "Jay";
+console.log(friends);
+
+const calcAge = function (birthYear) {
+    return 2025 -  birthYear;
+}
+
+const ages = [calcAge(2000), calcAge(1940, calcAge(1991))];
+console.log(ages);
+
+const newLength = friends.push("John");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("Mark");
+console.log(friends);
+
+//removing elements
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+const shifted = friends.shift();
+console.log(shifted);
+console.log(friends);
+
+// finding an element
+console.log(friends.indexOf("Jay"));
+console.log(friends.indexOf("Mark"));
+
+// includes
+console.log(friends.includes("Peter"));
+console.log(friends.includes("John"));
+
+//Array Iterations
+for (let i = 0; 1 < friends.length; index++) {
+   console.log(friends[i]);
+}
+
+friends.forEach( 
+    function(friend, index) {
+        console.log(`${index + 1}: ${friends} `);
+    }
+);
+
+friends.forEach(
+    (friends, index) => {
+        console.log(`${index + 1}: ${friends}`);
+    }
+);
+
+const grades2 = [85, 92, 78, 96, 88, 74];
+let total = 0;
+
+for (let i = 0; i < grades2.length; index++) {
+    total += grades2[i]
+}
+
+const average = total / grades2.length;
+console.log(`Average grades: ${average.toFixed(2)}`);
+
+let passedCount = 0;
+grades2.forEach((grade) => {
+     if (grade >= 70) passedCount++;
+    }
+);
